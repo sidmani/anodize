ef2120c3722d2bdfd61e291cc2f8fb0c62cfcb8e
Sid Mani <sidmani42@gmail.com>
2018-06-02 15:43:06 -0700

    serve and live reload

--- src/commands/watch.js
+++ src/commands/watch.js
@@ -3,6 +3,7 @@
 const fs = require('fs');
 const run = require('./run.js');
 const copy = require('./copy.js');
+const serve = require('./serve.js');
 
 exports.command = 'watch';
 exports.describe = 'Execute the generator each time the source or static directories change';
@@ -13,9 +14,27 @@ exports.builder = {
     boolean: true,
     default: false,
   },
+  'no-template': {
+    describe: 'Ignore changes in the template directory',
+    boolean: true,
+    default: false,
+  },
+  serve: {
+    describe: 'Serve HTTP from the target directory on the specified port',
+    boolean: true,
+    default: false,
+  },
+  port: {
+    describe: 'Port for serving HTTP',
+    default: 8000,
+  },
 };
 
 exports.handler = function handler(argv) {
+  if (argv.serve) {
+    serve.serve(argv.path.target, argv.port);
+  }
+
   console.log('Watching source directory...');
   if (!argv['no-static']) {
     fs.watch(argv.path.static, { recursive: true }, (eventType, filename) => {
@@ -24,13 +43,15 @@ exports.handler = function handler(argv) {
     });
   }
 
-  fs.watch(argv.path.template, { recursive: true }, (eventType, filename) => {
-    console.log(`Change in ${filename}, regenerating...`);
-    run.handler(argv);
-  });
+  if (!argv['no-template']) {
+    fs.watch(argv.path.template, { recursive: true }, (eventType, filename) => {
+      console.log(`Change in ${filename}, regenerating...`);
+      run.handler(argv, false);
+    });
+  }
 
   fs.watch(argv.path.source, { recursive: true }, (eventType, filename) => {
     console.log(`Change in ${filename}, regenerating...`);
-    run.handler(argv);
+    run.handler(argv, false);
   });
 };
